ce60c145015fd30ca5203460fe3778d002156053
Carlos Mañas <carlos@spacenomads.com>
2019-09-07 19:19:57 +0200

    Add playable version

--- src/index.js
+++ src/index.js
@@ -26,31 +26,45 @@ function getRandomWord(arr) {
 	return arr[index];
 }
 
-const HangMan = props => {
-	const {rope} = props;
-	return (
-		<div className={`app__hangman hangman ${rope ? 'hangman--with-rope':''}`}>
-			{rope && <div className="hangman__rope"></div>}
-      <div className="hangman__body">
-        <div className="hangman__head">
-          <div className="hangman__face hangman__face--sad"></div>
-        </div>
-        <div className="hangman__leg hangman__leg--left"></div>
-        <div className="hangman__leg hangman__leg--right"></div>
-      </div>
-		</div>
-	);
-};
+class HangMan extends React.Component {
+	isActive(limit) {
+		const {tries} = this.props;
+		let result = '';
+		if ( tries >= limit ) {
+			result = 'hangman__part--active';
+		}
+
+		return result;
+	}
+
+	render() {
+		const {rope, tries} = this.props;
+		return (
+			<div className={`app__hangman hangman ${rope ? 'hangman--with-rope':''}`}>
+				{rope && <div className={`hangman__rope ${this.isActive(1)}`}></div>}
+				<div className={`hangman__body ${this.isActive(2)}`}>
+					<div className={`hangman__head ${this.isActive(2)}`}>
+						<div className={`hangman__face ${(tries > 0) ? 'hangman__face--sad':''}`}></div>
+					</div>
+					<div className={`hangman__leg hangman__leg--left ${this.isActive(3)}`}></div>
+					<div className={`hangman__leg hangman__leg--right ${this.isActive(4)}`}></div>
+				</div>
+			</div>
+		);
+	}
+}
 
 class App extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
+			totalTries: 4,
 			tries: 0,
 			word: [],
 			hiddenWord: [],
 			userKey: '',
-			userHistory: []
+			userHistory: [],
+			gameOver: false
 		};
 		this.getUserKey = this.getUserKey.bind(this);
 	}
@@ -61,7 +75,7 @@ class App extends React.Component {
 
 	startGame() {
 		const word = getRandomWord(DEFAULTWORDS).split("");
-		const hiddenWord = word.map(letter => letter);
+		const hiddenWord = word.map(letter => '');
 		this.setState({
 			word: word,
 			hiddenWord: hiddenWord
@@ -69,16 +83,42 @@ class App extends React.Component {
 	}
 
 	getUserKey(event) {
-		const key = event.currentTarget.value;
-		
+		const key = event.currentTarget.value.toLowerCase();
 		if (key) {
 			this.setState(prevState => {
-				const newHistory = [...prevState.userHistory];
+				const {hiddenWord, userHistory, word, totalTries} = prevState;
+				let {tries, gameOver} = prevState;
+				const newHistory = [...userHistory];
+				const newHiddenWord = [...hiddenWord]
+				if (word.includes(key)) {
+					for (let i=0;i<word.length;i++) {
+						if (word[i] === key) {
+							newHiddenWord[i] = key;
+						}
+					}
+				} else {
+					if (tries < totalTries) {
+						tries++;
+						if (tries === totalTries) {
+							gameOver = true;
+						}
+					}
+
+				}
 				newHistory.push(key);
 				return {
+					tries: tries,
+					hiddenWord: newHiddenWord,
 					userKey: key,
-					userHistory: newHistory
+					userHistory: newHistory,
+					gameOver: gameOver
 				}
+			}, ()=>{
+				setTimeout(()=>{
+					this.setState({
+						userKey: ''
+					});
+				}, 500);
 			});
 		}
 	}
@@ -88,9 +128,10 @@ class App extends React.Component {
 	}
 
 	render() {
+		const {tries, userKey} = this.state;
 		return (
 			<React.Fragment>
-				<HangMan rope={true} />
+				<HangMan rope={true} tries={tries} />
 				<div className="app__ui">
 					<div className="app__word">
 						{this.state.hiddenWord.map((item, index) => {
@@ -103,7 +144,7 @@ class App extends React.Component {
 					</div>
 					<div className="app__user">
 						<div className="app__user-field">
-							<input className="app__user-letter" type="text" onChange={this.getUserKey} onClick={this.emptyField}maxLength="1"/>
+							<input className="app__user-letter" type="text" onChange={this.getUserKey} onClick={this.emptyField} maxLength="1" value={userKey}/>
 						</div>
 					</div>
 				</div>
