9de7e79db310b3f212c20172d3781ea94c5ff528
Carlos Mañas <carlos@spacenomads.com>
2019-09-07 18:03:55 +0200

    Add iu styles and basic code (not-playable)

---
+++ docs/precache-manifest.462882aa30f6953112ad691c7577b368.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12e63cd91d2dcf09de0082365354b966",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b24fcb6ee204b38e3303",
+    "url": "/static/css/main.ee462306.chunk.css"
+  },
+  {
+    "revision": "06e2ce356d4014c05142",
+    "url": "/static/js/2.e4dd051f.chunk.js"
+  },
+  {
+    "revision": "b24fcb6ee204b38e3303",
+    "url": "/static/js/main.131afe31.chunk.js"
+  },
+  {
+    "revision": "efe058d1b0a3c7d11f14",
+    "url": "/static/js/runtime~main.e2283daa.js"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,116 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.scss';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const DEFAULTWORDS = [
+	"rule",
+	"left",
+	"css",
+	"and",
+	"comb",
+	"this",
+	"get",
+	"html",
+	"api",
+	"from"
+];
+//const ENDPOINT = 'https://api.rand.fun/text/word';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function getRandomNumber(max) {
+	return Math.floor(Math.random() * max);
+}
+
+function getRandomWord(arr) {
+	const total = arr.length;
+	const index = getRandomNumber(total);
+	return arr[index];
+}
+
+const HangMan = props => {
+	const {rope} = props;
+	return (
+		<div className={`app__hangman hangman ${rope ? 'hangman--with-rope':''}`}>
+			{rope && <div className="hangman__rope"></div>}
+      <div className="hangman__body">
+        <div className="hangman__head">
+          <div className="hangman__face hangman__face--sad"></div>
+        </div>
+        <div className="hangman__leg hangman__leg--left"></div>
+        <div className="hangman__leg hangman__leg--right"></div>
+      </div>
+		</div>
+	);
+};
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			tries: 0,
+			word: [],
+			hiddenWord: [],
+			userKey: '',
+			userHistory: []
+		};
+		this.getUserKey = this.getUserKey.bind(this);
+	}
+	
+  componentDidMount() {
+		this.startGame();
+	}
+
+	startGame() {
+		const word = getRandomWord(DEFAULTWORDS).split("");
+		const hiddenWord = word.map(letter => letter);
+		this.setState({
+			word: word,
+			hiddenWord: hiddenWord
+		});
+	}
+
+	getUserKey(event) {
+		const key = event.currentTarget.value;
+		
+		if (key) {
+			this.setState(prevState => {
+				const newHistory = [...prevState.userHistory];
+				newHistory.push(key);
+				return {
+					userKey: key,
+					userHistory: newHistory
+				}
+			});
+		}
+	}
+
+	emptyField(event) {
+		event.currentTarget.value = '';
+	}
+
+	render() {
+		return (
+			<React.Fragment>
+				<HangMan rope={true} />
+				<div className="app__ui">
+					<div className="app__word">
+						{this.state.hiddenWord.map((item, index) => {
+							return (
+								<div key={index} className={`app__letter ${(item) ? 'app__letter--ok':''}`}>
+									{item}
+								</div>
+							);
+						})}
+					</div>
+					<div className="app__user">
+						<div className="app__user-field">
+							<input className="app__user-letter" type="text" onChange={this.getUserKey} onClick={this.emptyField}maxLength="1"/>
+						</div>
+					</div>
+				</div>
+			</React.Fragment>
+		);
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById("area"));
+
